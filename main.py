6d2b38cf4a2c29ddc486ee68d59405d01f7be0f7
“NITHISHKUMAR2398” <nithishkumar2398@gmail.com>
2024-02-07 11:44:45 +0530

    changes added

--- main.py
+++ main.py
@@ -2,7 +2,6 @@
 from input_module import input_text
 from pre_processing import remove_speaker_names
 
-# Call the preprocessing function on the input text
 processed_text = remove_speaker_names(input_text)
 
 # Print the processed text
