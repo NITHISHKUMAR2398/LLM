ca6c97e11b5421e23c2078870f3bf6bef35600ba
“NITHISHKUMAR2398” <nithishkumar2398@gmail.com>
2024-02-07 12:02:41 +0530

    changes added

--- pre_processing.py
+++ pre_processing.py
@@ -1,14 +1,11 @@
+# preprocess_module.py
 import re
 
-class PreprocessModule:
-    def __init__(self):
-        pass
+def remove_speaker_names(text):
+    # Define a regex pattern to match names with the format "Dr.[Name]:"
+    pattern = re.compile(r"Dr\.[A-Za-z]+:")
 
-    def remove_speaker_names(self, text):
-        # Define a regex pattern to match names with the format "Dr.[Name]:"
-        pattern = re.compile(r"Dr\.[A-Za-z]+:")
-
-        # Remove the matched names
-        processed_text = re.sub(pattern, "", text)
-        
-        return processed_text
+    # Remove the matched names
+    processed_text = re.sub(pattern, "", text)
+    
+    return processed_text
