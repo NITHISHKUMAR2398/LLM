aeaf680347d6308eba67fc70d340f28383007598
“NITHISHKUMAR2398” <nithishkumar2398@gmail.com>
2024-02-27 12:50:13 +0530

    final

--- app.py
+++ app.py
@@ -18,9 +18,9 @@ class ExtractTasks:
         data = {
             "model": "mistral",  # The model name (presumably for generating responses)
             "stream": False,  # Indicates whether to stream the response (set to False)
-            "prompt": f"Extract action tasks from the given conversation in the below format and add html break tags for each task:\n\
+            "prompt": f"Extract action tasks from the given conversation and add html break tags for each task:\n\
                         Based on the conversation, here are the action tasks that can be extracted:\n\
-                        Here is an example for you: Print the output in a similar way:<br>\n\
+                        please use the below tasks as an example for formatting the output and provide it in similar way:<br>\n\
                         1: Call Dr.Karthikeyan about the chart to be discussed.\n\
                         2: Check for documentaries on oral health in Netflix.\n\
                         3: Look for Oceania region in addition to Asian countries for further research.\n\
@@ -46,41 +46,23 @@ class TableToText:
 
     def table_to_text(self):
         excel_file_path = Config.EXCEL_FILE_PATH
-        df = pd.read_excel(excel_file_path)
-        table_data_str = df.to_string(index=False, header=True)
+        df = pd.read_csv(excel_file_path)
+        # table_data_str = df.to_string(index=False, header=True)
 
-        prompt = f"Consider all the rows and columns in the table and provide me a detailed description of the sheet in paragraph.\n\
-                   Based on the table data provided, here are some key points to include:\n\
-                        1. Describe the overall structure of the table.\n\
-                        2. Highlight any trends or patterns you observe in the data.\n\
-                        3. Identify any outliers or unusual data points.\n\
-                        4. Discuss any correlations between different columns.\n\
-                        5. Provide insights or conclusions drawn from the data.\n\
-            Here is an example below :\n\
-          **Paragraph 1:**\n\
-            Large Language Models (LLMs) represent a significant advancement in the field of natural language processing (NLP).\n\
-            These models, such as GPT-3 developed by OpenAI, are trained on vast amounts of text data and have the capability to generate human-like text in\n\
-            response to prompts or queries. LLMs employ deep learning techniques, particularly transformer architectures,\n\
-            which allow them to understand and generate text with remarkable accuracy and coherence. With their\n\
-            immense size and complexity, LLMs can comprehend and produce text across a wide range of topics and\n\
-            writing styles. Their versatility and ability to adapt to different tasks make them invaluable tools\n\
-            for various applications, including content generation, language translation, and conversational agents.\n\
-          \n\
-           \n\
-            \n\
-          **Paragraph 2:**\n\
-            The development and deployment of Large Language Models (LLMs) have sparked both excitement and debate within the\n\
-            scientific community and society at large. On one hand, LLMs represent a remarkable achievement in\n\
-            artificial intelligence, pushing the boundaries of what machines can accomplish in understanding and\n\
-            generating human language. Their potential applications in areas such as education, healthcare, and\n\
-            creative writing hold promise for improving efficiency and enhancing human experiences. However,\n\
-            concerns have been raised regarding the ethical implications and potential risks associated with LLMs,\n\
-            including biases in the training data, misinformation propagation, and job displacement. As researchers\n\
-            and policymakers grapple with these challenges, it is crucial to ensure responsible development and\n\
-            deployment of LLMs to harness their benefits while mitigating potential harms.\n\
-            {table_data_str}"
+        prompt =  f"Given the dataframe df, conduct a thorough analysis of the data to provide a detailed description of the dataset within a 300-word limit.\n\
+                   Key points to include based on the table data:\n\
+                   1. Offer a comprehensive overview of the table's structure, including the number of rows and columns, data types, and any missing values.\n\
+                   2. Analyze trends or patterns within the data, such as temporal trends, spatial patterns, or recurring patterns in categorical variables.\n\
+                   3. Identify and discuss any outliers or unusual data points that deviate significantly from the norm, considering their potential impact on the analysis.\n\
+                   4. Explore potential correlations between different columns and their implications for understanding the relationships between variables.\n\
+                   5. Evaluate the quality and integrity of the data, addressing any data quality issues and potential biases.\n\
+                   6. Offer insightful conclusions drawn from the data analysis, highlighting key findings and their implications for decision-making.\n\
+                   7. Provide recommendations for further exploration or research based on the analysis, suggesting potential avenues for enhancing understanding or addressing unanswered questions.\n\
+                      \n\{df}"    
 
 
+            
+        
         data = {
             "model": "mistral",
             "prompt": prompt,
@@ -100,22 +82,21 @@ class GenerateMCQs:
 
     def generate_mcqs(self):
         excel_file_path = Config.EXCEL_FILE_PATH
-        df = pd.read_excel(excel_file_path)
-        table_data_str = df.to_string(index=False)
+        df = pd.read_csv(excel_file_path)
+        #table_data_str = df.to_string(index=False)
 
-        prompt = f"Generate 5 random Multiple choice question with 4 options and display it out of which 1 is the correct option with reason by analyzing the  data in the below format: \n\
-            and add html break tags for each question and answer. Here is an example for you.\n\
-            Question 1:<br>\n\
-            Which country has the highest hourly wage? <br>\n\
-            A) Czech Republic <br>\n\
-            B) Denmark<br>\n\
-            C) Switzerland<br>\n\
-            D) Hungary<br>\n\
-               answer : Switzerland (The new hourly wage for the Switzerland is the highest in this data set.)<br>\n\
-                {table_data_str}"
-            
-            
-
+        prompt = f"Given the dataframe df, thoroughly analyze the data to create five meticulously crafted multiple-choice questions (MCQ), each with four options, out of which only one is the correct answer.\n\
+                    Exclude columns deemed least important from consideration in MCQ generation.\n\
+                    Utilize the provided MCQ format for structuring the output as shown below:\n\
+                    Question 1:<br>\n\
+                    Which country has the highest hourly wage? <br>\n\
+                    A) India  <br>\n\
+                    B) Denmark<br>\n\
+                    C) Switzerland<br>\n\
+                    D) None of the above<br>\n\
+                    Answer : Switzerland (Switzerland holds the highest hourly wage in this dataset, standing at 17.50.)<br>\n\
+                    {df}"
+        
         data = {
             "model": "mistral",
             "prompt": prompt,
@@ -125,6 +106,8 @@ class GenerateMCQs:
 
         if response.status_code == 200:
             print(json.loads(response.text)['response'])
+            
+            
             return json.loads(response.text)['response']
         else:
             return jsonify({"error": f"Request failed with status code {response.status_code}"})
